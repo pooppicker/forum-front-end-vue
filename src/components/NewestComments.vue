abf3de26065f242efa1b069fc128e8ffce5372c7
Wendy Tsai <>
2021-08-25 14:25:41 +0800

    feat: add feeds

---
+++ src/components/NewestComments.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="card">
+    <div class="card-header">最新評論</div>
+    <div class="card-body">
+      <div
+        v-for="comment in comments"
+        :key="comment.id"
+      >
+        <h4>
+          <a href="#"> {{ comment.Restaurant.name}} </a>
+        </h4>
+        <p>{{ comment.text }}</p>
+        by
+        <a href="#"> {{ comment.User.name}} </a>
+        at {{ comment.createdAt | fromNow() }} days ago
+        <hr />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import moment from 'moment'
+export default {
+  filters: {
+    fromNow(value) {
+      if (!value) {
+        return '-'
+      }
+      return moment(value).fromNow()
+    }
+  },
+  props: {
+    comments: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
--- src/components/NewestRestaurants.vue
+++ src/components/NewestRestaurants.vue
@@ -4,41 +4,45 @@
       最新餐廳
     </div>
     <div class="card-body">
-      <div>
+      <div 
+        v-for="restaurant in restaurants"
+        :key="restaurant.id"
+      >
         <h4>
-          <a href="#">Deangelo Farrell</a>
-          <small>中式料理</small>
+          <a href="#">{{ restaurant.name }}</a>
+          <small>{{ restaurant.Category.name}}</small>
         </h4>
-        <p>Voluptates veritatis blanditiis ipsam. Tempora molestiae ut qui deserunt debitis sit. Impedit est rem. Blanditiis aliquid atque quo necessitatibus. Veniam sint explicabo. Est velit soluta.</p>11 days ago
-        <hr>
-      </div>
-      <div>
-        <h4>
-          <a href="#">Carter Howe</a>
-          <small>日本料理</small>
-        </h4>
-        <p>Voluptates veritatis blanditiis ipsam. Tempora molestiae ut qui deserunt debitis sit. Impedit est rem. Blanditiis aliquid atque quo necessitatibus. Veniam sint explicabo. Est velit soluta.</p>11 days ago
-        <hr>
-      </div>
-      <div>
-        <h4>
-          <a href="#">Dr. Carole Smith</a>
-          <small>義大利料理</small>
-        </h4>
-        <p>Voluptates veritatis blanditiis ipsam. Tempora molestiae ut qui deserunt debitis sit. Impedit est rem. Blanditiis aliquid atque quo necessitatibus. Veniam sint explicabo. Est velit soluta.</p>11 days ago
-        <hr>
-      </div>
-      <div>
-        <h4>
-          <a href="#">Nyah Bernier</a>
-          <small>素食料理</small>
-        </h4>
-        <p>Voluptates veritatis blanditiis ipsam. Tempora molestiae ut qui deserunt debitis sit. Impedit est rem. Blanditiis aliquid atque quo necessitatibus. Veniam sint explicabo. Est velit soluta.</p>11 days ago
+        <p>
+          {{ restaurant.description }}
+        </p>
+        {{ restaurant.createdAt | fromNow() }}
         <hr>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+import moment from 'moment'
+
+export default {
+  filters: {
+    fromNow(value) {
+      if (!value) {
+        return '-'
+      }
+      return moment(value).fromNow()
+    }
+  },
+  props: {
+    restaurants: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
 
 <style scoped>
 small {
