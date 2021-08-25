705ba2e1111d60376df3254dabf4fc11a93caaac
Wendy Tsai <>
2021-08-25 11:52:16 +0800

    project init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="restaurant-forum">
+      <Navbar />
+      <main class="mt-5 bg-white">
+        <router-view/>
+      </main>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
+import Navbar from './components/Navbar'
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    Navbar,
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
